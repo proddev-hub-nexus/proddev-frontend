56500eea3217cb95317c799bf84c961fd2d9718a
Proddev Hub <proddevhubnexus@gmail.com>
2025-08-06 23:15:51 +0200

    fixed tailwindcss

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,7 @@ import tailwindcssAnimate from "tailwindcss-animate";
 import typography from "@tailwindcss/typography";
 
 const config: Config = {
-  darkMode: ["class"],
+  darkMode: "class",
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
